89bb5713dc9da5bf3b3546fa42ec5f90bac3c5b5
Your Name <you@example.com>
2024-03-20 11:49:11 +0530

    first commit

---
+++ proj/src/components/Home.jsx
@@ -0,0 +1,85 @@
+
+
+export const Home = () => {
+  return (
+    <div className="container">
+        <div className="child_cont">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Products<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Search</a>
+      </li>
+     
+     
+    </ul>
+  </div>
+</nav>
+        
+        </div>
+        <div className="contDesign">
+        <div className="card1" id="shoe">
+        <div className="imagecont">
+
+        </div>
+        <div className="textcont">
+
+        </div>
+
+     </div>
+     <div className="card1" id="shirt">
+        <div className="imagecont">
+
+        </div>
+        <div className="textcont">
+
+        </div>
+
+     </div>
+     <div className="card1" id="shirt">
+        <div className="imagecont">
+
+        </div>
+        <div className="textcont">
+        
+        </div>
+
+     </div>
+        </div>
+        <div className="contDesign">
+        <div className="card1" id="shoe">
+        <div className="imagecont">
+
+        </div>
+        <div className="textcont">
+
+        </div>
+
+     </div>
+     <div className="card1" id="pant">
+        <div className="imagecont">
+
+        </div>
+        <div className="textcont">
+
+        </div>
+
+     </div>
+     <div className="card1">
+        <div className="imagecont">
+
+        </div>
+        <div className="textcont">
+
+        </div>
+
+     </div>
+        </div>
+  
+    </div>
+  )
+}
